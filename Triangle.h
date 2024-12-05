97d1c49edc349c594441b22c9c755669b4abb707
Jens Steenmetz <jj.steenmetz@gmail.com>
2024-12-05 11:05:15 +0100

    Format Triangle.h and remove geometry.h

--- Triangle.h
+++ Triangle.h
@@ -18,16 +18,24 @@ struct Triangle
 		const glm::vec3 edge2 = vertex2 - vertex0;
 		const glm::vec3 h = glm::cross(direction, edge2);
 		const float a = glm::dot(edge1, h);
+
 		if (a > -0.0001f && a < 0.0001f) return false; // ray parallel to triangle
+
 		const float f = 1 / a;
 		const glm::vec3 s = origin - vertex0;
 		const float u = f * glm::dot(s, h);
+
 		if (u < 0 || u > 1) return false;
+
 		const glm::vec3 q = glm::cross(s, edge1);
 		const float v = f * glm::dot(direction, q);
+
 		if (v < 0 || u + v > 1) return false;
+
 		const float t = f * glm::dot(edge2, q);
-		if (t > 0.0001f) {
+
+		if (t > 0.0001f)
+		{
 			tnear = tnear < t ? tnear : t;
 			return true;
 		}
