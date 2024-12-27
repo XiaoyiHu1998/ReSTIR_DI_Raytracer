255b0c624c976f1b13b42ebc552d56f1130266af
Xiao yi Hu <xy1998@live.nl>
2024-12-27 14:16:10 +0100

    Added a Settings.h file for global settings that can be used by imgui

---
+++ Assignment 2/Sandbox/src/Settings.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Settings
+{
+	// Add settings to be used globally in program and ImGui
+
+	// Rendering Resolution (independent from imgui viewport size)
+	// uint32_t Rendering Kernel size
+	// uint32_t Samples per pixel
+	// enum Render paths
+
+}
