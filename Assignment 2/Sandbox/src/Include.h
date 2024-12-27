e6726edd705719419e4e6175031f559ae3555174
Xiao yi Hu <xy1998@live.nl>
2024-12-27 16:25:51 +0100

    Added extra includes in Include.h to add glm vector string conversions, did require enabling experimental glm features

--- Assignment 2/Sandbox/src/Include.h
+++ Assignment 2/Sandbox/src/Include.h
@@ -1,8 +1,11 @@
 #include <stdint.h>
 #include <vector>
 #include <memory>
+#include <iostream>
 
+#define GLM_ENABLE_EXPERIMENTAL
 #include "glm/glm.hpp"
 #include "glm/ext.hpp"
+#include <glm/gtx/string_cast.hpp>
 
 using FrameBufferRef = std::shared_ptr<std::vector<unsigned char>>;
