a1cbef1f991bb0654724be72f6f5e4a90466c5d6
Jens Steenmetz <jj.steenmetz@gmail.com>
2024-12-30 16:29:59 +0100

    change render settings

--- Assignment 2/Sandbox/src/Settings.h
+++ Assignment 2/Sandbox/src/Settings.h
@@ -7,8 +7,8 @@ namespace Settings
 	// Add settings to be used globally in program and ImGui
 
 	// Rendering Resolution (independent from imgui viewport size)
-	uint32_t RenderingKernelSize;
-	uint32_t ThreadCount = 4;
+	uint32_t RenderingKernelSize = 16;
+	uint32_t ThreadCount = 10;
 	// uint32_t Samples per pixel
 	// enum Render paths
 
