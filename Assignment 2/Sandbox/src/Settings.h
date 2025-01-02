c8f283fd15ed9348760ac011ee35ee1d6c849bb0
Xiao yi Hu <xy1998@live.nl>
2025-01-02 11:46:11 +0100

    Updated Settings to include ThreadCount, added RenderResolutionWidth, RenderResolutionHeight, SamplesPerPixel and MaxRayDepth

--- Assignment 2/Sandbox/src/Settings.h
+++ Assignment 2/Sandbox/src/Settings.h
@@ -1,15 +1,19 @@
 #pragma once
 
+#include <thread>
+
 #include "Include.h"
 
 namespace Settings
 {
 	// Add settings to be used globally in program and ImGui
+	uint32_t ThreadCount = std::thread::hardware_concurrency() - 2;
 
-	// Rendering Resolution (independent from imgui viewport size)
+	uint32_t RenderResolutionWidth = 640;
+	uint32_t RenderResolutionHeight = 480;
 	uint32_t RenderingKernelSize = 16;
-	uint32_t ThreadCount = 10;
-	// uint32_t Samples per pixel
-	// enum Render paths
+	uint32_t SamplePerPixel = 1;
+
+	uint32_t MaxRayDepth = 8;
 
 }
