7a35a8e753218111930782e9631fcca55146e489
Xiao yi Hu <xy1998@live.nl>
2025-01-11 19:51:57 +0100

    Added ReSTIR.h and ReSTIR.cpp for future ReSTIR specific function and classes implementation

---
+++ Assignment 2/Sandbox/src/ReSTIR.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "Include.h"
+#include "Utils.h"
+
+namespace ReSTIR
+{
+	//using T = glm::vec3;
+
+	template <class T>
+	class Resevoir
+	{
+	private:
+		T m_Sample;
+		float m_WeightTotal;
+
+		int m_SampleCount;
+	public:
+		Resevoir(T initialSample, float weight):
+			m_Sample{ initialSample }, m_SampleCount{ 1 }, m_WeightTotal { weight }
+		{}
+
+		T GetSample() { return m_Sample; }
+
+		bool Update(const T& sample, float weight, uint32_t& seed)
+		{
+			m_SampleCount++;
+			m_WeightTotal += weight;
+
+			if (Utils::RandomFloat(seed) < weight / m_WeightTotal)
+			{
+				m_Sample = sample;
+				return true;
+			}
+
+			return false;
+		}
+	};
+
+
+	Resevoir<glm::vec3> ResevoirSampling(const glm::vec3& sample, int candidateCount, uint32_t& seed)
+	{
+		Resevoir<glm::vec3> resevoir(glm::vec3(0), 1.0f / candidateCount);
+
+		for (int i = 0; i < candidateCount; i++)
+		{
+			resevoir.Update(sample, 1.0f / candidateCount, seed);
+		}
+
+
+		return resevoir;
+	}
+
+}
