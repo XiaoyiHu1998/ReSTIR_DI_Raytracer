711e11355c99d2e298bf032e37ab36c4f28d3c14
Xiao yi Hu <xy1998@live.nl>
2025-07-06 01:16:45 +0200

    Removed Assignment 1 and moved Assignment 2 to repository root folder

--- Assignment 1/AccelerationStructure.cpp
+++
@@ -1,2 +0,0 @@
-#include "AccelerationStructure.h"
-
--- Assignment 1/RenderObject.cpp
+++
@@ -1,9 +0,0 @@
-#include "RenderObject.h"
-
-bool RenderObject::intersect(const glm::vec3& orig, const glm::vec3& dir, float& tnear)
-{
-	glm::mat4 invTransform = glm::inverse(m_Transform.GetTransformMatrix());
-	glm::vec3 origLocal = glm::vec3(invTransform * glm::vec4(orig, 1));
-	glm::vec3 dirLocal = glm::vec3(invTransform * glm::vec4(dir, 0));
-	return m_Model.intersect(origLocal, dirLocal, tnear);
-}
